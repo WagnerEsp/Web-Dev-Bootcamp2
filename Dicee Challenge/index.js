9f2e383d32fcfc1e6d6d766a5036c34c66c01f12
Wager Espinoza <wagner.espinoza@gmail.com>
2023-01-05 11:52:39 +0100

    add btn to play and @media max-w 576px

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -10,11 +10,19 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+    document.querySelector("h1").innerHTML = "2 🚩 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Play 2 Wins! 🚩";
+    document.querySelector("h1").innerHTML = "1 🚩 Wins!";
 }
 else {
-    document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector("h1").innerHTML = "🚩 Draw! 🚩";
 }
+
+const reloadWeb = document.querySelector('.reloadButton');
+reloadWeb.onclick = function() {miFuncion()};
+
+
+function miFuncion() {
+  window.location.reload();
+};
